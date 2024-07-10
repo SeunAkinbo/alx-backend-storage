1d92d39ac508567151eda9b30224c869cb9d350f
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-10 10:31:32 +0000

    Creating SQL Table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script that creayes a table users with
+-- id, integer, never null, auto increament and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- if the table exists the script should not fail
+-- Executable on any database
+
+
