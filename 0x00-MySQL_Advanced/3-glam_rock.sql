447855ec52f373bd784c3a679fea2264dfa6c3dd
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-10 16:08:46 +0000

    Glam Rock Query

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,16 @@
+-- Task: List all bands with Glam rock as their main style, ranked by their longevity
+-- Context: Calculate/compute something is always power intensive… better to distribute the load!
+
+-- List bands with Glam rock as their main style, ranked by their longevity
+SELECT 
+    band_name,
+    CASE
+	WHEN split IS NULL THEN 2022 - formed
+	ELSE split - formed
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    style LIKE '%Glam rock%'
+ORDER BY 
+    lifespan DESC;
