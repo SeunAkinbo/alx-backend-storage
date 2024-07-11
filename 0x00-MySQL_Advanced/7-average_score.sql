4ff0c1d56323ee50bac79572517455aac0f788cc
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 20:09:37 +0000

    Procedure Average Score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,7 +5,7 @@ DELIMITER //
 
 -- Create the stored procedure ComputeAverageScoreForUser
 CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT
+    IN new_user_id INT
 )
 BEGIN
     DECLARE avg_score DECIMAL(10, 2);
@@ -13,12 +13,12 @@ BEGIN
     -- Calculate the average score for the user
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = new_user_id;
 
     -- Insert or update the average score in the user_average_scores table
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE id = new_user_id;
 END //
 
 DELIMITER ;
