1d85a8ea1ab6c936c75ca0d503076c3a03c33718
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 17:12:01 +0000

    Unique Users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,14 +1,6 @@
--- Task: Create a table 'users' with specified attributes and constraints
--- Context: Make an attribute unique directly in the table schema to enforce business rules and avoid bugs in your application
-
 -- Check if the table 'users' already exists and create it if it doesn't
 CREATE TABLE IF NOT EXISTS users (
-	-- Attribute: id, integer, never null, auto increament and primary key
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-
-	-- Attribute: email, string (255 characters), never null, auto increment and primary key
 	email VARCHAR(255) NOT NULL UNIQUE,
-
-	-- Attribute: name, string (255 characters)
 	name VARCHAR(255)
 );
