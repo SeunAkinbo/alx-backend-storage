08a39d4cc9365be03fd196eb584205690a940a2a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 19:17:35 +0000

    Unique Users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,14 @@
--- a SQL script that creates a table users following requirements
+-- Task: Create a table 'users' with specified attributes and constraints
+-- Context: Make an attribute unique directly in the table schema to enforce business rules and avoid bugs in your application
 
-CREATE TABLE IF NOT EXISTS users(
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+-- Check if the table 'users' already exists and create it if it doesn't
+CREATE TABLE IF NOT EXISTS users (
+	-- Attribute: id, integer, never null, auto increament and primary key
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+	-- Attribute: email, string (255 characters), never null, auto increment and primary key
+	email VARCHAR(255) NOT NULL UNIQUE,
+
+	-- Attribute: name, string (255 characters)
+	name VARCHAR(255)
 );
