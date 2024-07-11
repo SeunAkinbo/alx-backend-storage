57987ed1e7dcc1c9a62392ae7067c9e554fdf959
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 21:16:53 +0000

    Indexing Name Score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,8 +1,5 @@
 -- Task: Create an index idx_name_first_score on the table names and the first letter of name and the score
 -- Context: Index is not the solution for any performance issue, but well used, it’s really powerful!
 
--- Drop the existing index if it exists
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
 -- Create an index on the first letter of the name column and the score column
-CREATE INDEX idx_name_first_score ON names (SUBSTRING(name, 1, 1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score)
