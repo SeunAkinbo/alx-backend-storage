04637e39424a57544d8a23c5ce174ebdfc10231c
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 20:33:49 +0000

    Indexing

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,8 @@
 -- SQL script that creates an index idx_name_first on the table names and the first letter of name.
 -- Requirements: Only the first letter of name must be indexed
 
+-- Drop the existing index if it exists
+DROP INDEX IF EXISTS idx_name_first ON names;
+
 -- Create index idx_name_first on the table names for the first letter of name
 CREATE INDEX idx_name_first ON names (name(1));
