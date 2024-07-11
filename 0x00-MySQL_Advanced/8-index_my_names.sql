5b87eafed065ab3ea16592df1003804d0a4348e1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 21:07:17 +0000

    Indexing

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -5,4 +5,4 @@
 DROP INDEX IF EXISTS idx_name_first ON names;
 
 -- Create an index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
+CREATE INDEX idx_name_first ON names (name(1));
