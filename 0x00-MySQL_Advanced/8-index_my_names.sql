48f9cdf66ed8021af29540468b62addddadf7a6a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-11 21:03:53 +0000

    Indexing

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,8 +1,8 @@
--- SQL script that creates an index idx_name_first on the table names and the first letter of name.
--- Requirements: Only the first letter of name must be indexed
+-- Task: Create an index idx_name_first on the table names and the first letter of name
+-- Context: Index is not the solution for any performance issue, but well used, it’s really powerful!
 
 -- Drop the existing index if it exists
 DROP INDEX IF EXISTS idx_name_first ON names;
 
--- Create index idx_name_first on the table names for the first letter of name
-CREATE INDEX idx_name_first ON names (name(1));
+-- Create an index on the first letter of the name column
+CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
