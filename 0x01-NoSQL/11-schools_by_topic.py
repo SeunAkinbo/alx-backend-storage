7b687e05f41b056d7b6b2944065b706202f52692
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-19 23:21:50 +0000

    School by Topics

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module - 11-school_by_topic.py"""
+
+
+def school_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+
+    :param mongo_collection: pymongo collection object
+    :param topic: string, the topic to search for
+    :return: list of schools having the specific topic
+    """
+    return [mongo_collection.find({"topics": topic})]
