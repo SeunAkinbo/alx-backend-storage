bf4a1699446627a39929a8a2294ac20b2d336e65
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-19 23:36:38 +0000

    School by Topics

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -10,9 +10,4 @@ def school_by_topic(mongo_collection, topic):
     :param topic: string, the topic to search for
     :return: list of schools having the specific topic
     """
-    topic_filter = {
-        'topics': {
-            '$elemMatch': {'$eq': topic},
-            },
-    }
-    return [doc for doc in mongo_collection.find(topic_filter)]
+    return list(mongo_collection.find({'topics': topic}))
