312a77868b290a2ce7ddbccf2c4619717eca7691
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-19 22:47:21 +0000

    List All

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Module - 8-all.py"""
-from pymongo import MongoCLient
 
 
 def list_all(mongo_collection):
@@ -10,5 +9,4 @@ def list_all(mongo_collection):
     :param mongo_collection: pymongo collection object
     :return: list of documents in the collection, or an empty list if no documents
     """
-    docs = list(mongo_collection.find())
-    return docs if docs else []
+    return [doc for doc in mongo_collection.find]
