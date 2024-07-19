b19c431b325ffd5f50edfc6de20f3fc0780b4441
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-19 23:48:37 +0000

    Students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -11,13 +11,23 @@ def top_students(mongo_collection):
     A list of dictionaries, each containing the student's original data
     and their average score.
     """
-    result = []
-    students = mongo_collection.find({})
-    for student in students:
-        topics = student.get("topics", [])
-        average = sum(i['score'] for i in topics) / len(topics) if topics else 0
-        student['averageScore'] = average
-        result.append(student)
-
-    sorted_result = sorted(result, key=lambda x: x['averageScore'], reverse=True)
-    return result
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
